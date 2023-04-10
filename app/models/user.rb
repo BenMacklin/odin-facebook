05edf974025232f8d3ad4502941d639bf6030ec6
BenMacklin <benjaminmacklin92@gmail.com>
2023-04-10 14:57:26 -0400

    Added sign in and out to home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  default_scope -> { order(created_at: :desc) }
 end
